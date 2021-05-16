c750fef9a753b2fee056713c71bb8750e82be5f0
Vick <victordickson474@gmail.com>
2021-05-16 19:27:36 +0100

    new commit

--- NeighborlyAPI/getPost/__init__.py
+++ NeighborlyAPI/getPost/__init__.py
@@ -10,9 +10,10 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
     if id:
         try:
-            url = "localhost"  # TODO: Update with appropriate MongoDB connection information
+            url = "mongodb://proj2cosmodb:EtUT0Q7AIfht2tPLL12k7pA4mMugSoWbvTsT6p5jI06zolTKoKZDLsFajNp2yPHSO0r1v3QQYLCkS4wJlF95WA==@proj2cosmodb.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@proj2cosmodb@
+"  # TODO: Update with appropriate MongoDB connection information
             client = pymongo.MongoClient(url)
-            database = client['azure']
+            database = client['proj2db']
             collection = database['posts']
 
             query = {'_id': ObjectId(id)}
--- NeighborlyFrontEnd/settings.py
+++ NeighborlyFrontEnd/settings.py
@@ -12,8 +12,8 @@ API_VERSION = 'v1'
 
 #-------- Azure constants
 
-# API_URL format: "https://[FUNCTION_APP_NAME_GOES_HERE].azurewebsites.net"
-#API_URL = " https://neighborlyapi.azurewebsites.net/api/"
+#API_URL format: "https://proj2funcapp.azurewebsites.net"
+API_URL = " https://proj2funcapp.azurewebsites.net/api/"
 
 # for local host if Azure functions served locally
 API_URL = "http://localhost:7071/api"
