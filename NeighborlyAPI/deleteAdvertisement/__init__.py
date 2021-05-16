288da14e92f90196c3139fc9ebc8ff47ea2945c2
Vick <victordickson474@gmail.com>
2021-05-17 00:01:48 +0100

    First commit

--- NeighborlyAPI/deleteAdvertisement/__init__.py
+++ NeighborlyAPI/deleteAdvertisement/__init__.py
@@ -9,7 +9,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
     if id:
         try:
-            url = "mongodb://proj2cosmodb:EtUT0Q7AIfht2tPLL12k7pA4mMugSoWbvTsT6p5jI06zolTKoKZDLsFajNp2yPHSO0r1v3QQYLCkS4wJlF95WA==@proj2cosmodb.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@proj2cosmodb@
+            url = "mongodb://proj2cosmosdb:21TvTDpAS4EFSVsWs4MfDE77oQsBQS8syJH06jX07QqJs0d5MWU4fFjdGICtdsrTGCM9p7KAOcpyWWseIZMHbg==@proj2cosmosdb.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@proj2cosmosdb@
 "  # TODO: Update with appropriate MongoDB connection information
             client = pymongo.MongoClient(url)
             database = client['proj2db']
