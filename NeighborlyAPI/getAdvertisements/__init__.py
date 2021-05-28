03c970d32e7606e8308357475d4dcba7298ccb60
Vick <victordickson474@gmail.com>
2021-05-28 15:35:44 +0100

    first commit

--- NeighborlyAPI/getAdvertisements/__init__.py
+++ NeighborlyAPI/getAdvertisements/__init__.py
@@ -6,10 +6,10 @@ from bson.json_util import dumps
 def main(req: func.HttpRequest) -> func.HttpResponse:
 
     try:
-        url = "mongodb://proj2cosmosdb:21TvTDpAS4EFSVsWs4MfDE77oQsBQS8syJH06jX07QqJs0d5MWU4fFjdGICtdsrTGCM9p7KAOcpyWWseIZMHbg==@proj2cosmosdb.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@proj2cosmosdb@
+        url = "mongodb://proj2cosmodb:MTYgw0J2gTKidkXGlh3oDPFAxcseJZBuyOamN8BxqJC9rKv6JvzlGWphsOqqwYDMCVK8TaWnqrCATm7jzlJ1dQ==@proj2cosmodb.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@proj2cosmodb@"
 "  # TODO: Update with appropriate MongoDB connection information
         client = pymongo.MongoClient(url)
-        database = client['proj2db']
+        database = client['proj2appdb']
         collection = database['advertisements']
 
 
