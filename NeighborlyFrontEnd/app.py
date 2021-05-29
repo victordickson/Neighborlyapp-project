834231db49fd0e61a032db5907dbd202da7beb68
Dickson Victor <victordickson474@gmail.com>
2021-05-29 22:26:27 +0100

    Update app.py

--- NeighborlyFrontEnd/app.py
+++ NeighborlyFrontEnd/app.py
@@ -46,7 +46,7 @@ def rss():
     fg = FeedGenerator()
     fg.title('Feed title')
     fg.description('Feed Description')
-    fg.link(href='https://myneighborlyapp.azurewebsites.net/')
+    fg.link(href='https://myproj2funcapp.azurewebsites.net/api/getnotes')
     
 
     response = requests.get(settings.API_URL + '/getAdvertisements')
