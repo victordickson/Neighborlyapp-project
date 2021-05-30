1c54d46387c94e6c3b8a941b8d2750f083ec4f6e
Vick <victordickson474@gmail.com>
2021-05-31 00:05:07 +0100

    updates

--- NeighborlyAPI/eventHubTrigger/__init__.py
+++ NeighborlyAPI/eventHubTrigger/__init__.py
@@ -1,25 +1,23 @@
-import json
-import logging
 import azure.functions as func
+import pymongo
+import os
+import json
+from bson.json_util import dumps
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+
+    try:
+        url = ""
+        client = pymongo.MongoClient(url)
+        database = ""
+        collection = ""
 
 
-def main(event: func.EventGridEvent):
+        result = collection.find({})
+        result = dumps(result)
 
-	logging.info('Function triggered to process a message: ', event.get_body())
-    logging.info('  EnqueuedTimeUtc =', event.enqueued_time)
-    logging.info('  SequenceNumber =', event.sequence_number)
-    logging.info('  Offset =', event.offset)
-
-    result = json.dumps({
-        'id': event.id,
-        'data': event.get_json(),
-        'topic': event.topic,
-        'subject': event.subject,
-        'event_type': event.event_type,
-    })
-
-
-    logging.info('Python EventGrid trigger processed an event: %s', result)
-
-
+        return func.HttpResponse(result, mimetype="application/json", charset='utf-8')
+    except:
+        return func.HttpResponse("Could not connect to mongodb",
+                                 status_code=400)
 
