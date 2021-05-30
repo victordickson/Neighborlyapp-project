83d2d21b3d25e1de4878854e5724155fe8d28c09
Vick <victordickson474@gmail.com>
2021-05-30 21:56:47 +0100

    updates

--- NeighborlyFrontEnd/venv/Scripts/Activate.ps1
+++ NeighborlyFrontEnd/venv/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="E:\WEB\Flask\Azure_function\venv"
+$env:VIRTUAL_ENV="C:\Neighborlyapp-project\Neighborlyapp-project\NeighborlyFrontEnd\venv"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
--- NeighborlyFrontEnd/venv/Scripts/activate.bat
+++ NeighborlyFrontEnd/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=E:\WEB\Flask\Azure_function\venv
+set VIRTUAL_ENV=C:\Neighborlyapp-project\Neighborlyapp-project\NeighborlyFrontEnd\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
